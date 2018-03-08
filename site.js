2c438e84e8b5d7f426a9a720e56f0642c5e95ccc
Christian Bourlier <chrisbourlier@hotmail.com>
2018-03-08 15:27:12 -0700

    sets and removes favorties from local storage

--- site.js
+++ site.js
@@ -1,44 +1,76 @@
 $(function() {
+  let apiBaseSearchURL = "https://g-food2fork.herokuapp.com/api/search?key=663114731c111ec95ac24dc309ea0ad9&count=24&q="
 
-  let requestURL = "https://g-food2fork.herokuapp.com/api/search?key=663114731c111ec95ac24dc309ea0ad9&count=24&q="
 let foodSearch = $("#foodSearch")
-  $("#search-form").submit(function(e) {
+  $("#search-form").submit((e) => {
     e.preventDefault()
+    $("#container-recipes").empty()
     foodSearch = $("#foodSearch").val()
-    requestURL = requestURL.concat(foodSearch)
+    requestURL = apiBaseSearchURL.concat(foodSearch)
     goToFoodSearch(requestURL)
   })
 
-
-
 })
 
-const goToFoodSearch = function(requestURL) {
-  fetch(requestURL).then(function(response) {
+const goToFoodSearch = (requestURL) => {
+  fetch(requestURL).then((response) => {
     return response.json()
-  }).then(function(data) {
+  }).then((data) => {
     makeRecipeCards(data)
-  }).catch(function() {
+  }).catch(() => {
     console.log("Chuck Norris was here")
   })
 }
 
-const makeRecipeCards = function (data) {
-  data.recipes.map(function (e) {
+const makeRecipeCards = (data) => {
+  data.recipes.map((e) => {
     let recipeName = e.title
     let recipeImg = e.image_url
     let recipeURL = e.source_url
 
     let recipeElement =
     `<div class="recipe">
-    <a href="${recipeURL}" target="_blank">
-      <div><img src="${recipeImg}"></div>
+      <div id="${recipeURL}"><img src="${recipeImg}"></div>
       <div class="recipe-title-header">
-        <h4 class="recipe-title">${recipeName}</h4></a>
+        <i class="far fa-star"></i>
+        <h4 class="recipe-title">
+          <a href="${recipeURL}" target="_blank">${recipeName}</a>
+        </h4>
       </div><br>`
 
     $('#container-recipes').append(recipeElement)
+  })
 
+  // everything appended
+  $('.recipe .fa-star').click((e) => {
+    $(e.target).toggleClass('far fas')
 
+    let aEle = $(e.target).next().find('a')
+    let title = aEle.text()
+    let link = aEle.attr('href')
+    let image = $(e.target).parent().prev().find('img').prop('src')
+    let faves = JSON.parse(localStorage.getItem('favorites') || '[]')
+
+    // create obj for the recipe, with the data above
+    let obj = {
+      'title': title,
+      'link': link,
+      'image': image
+    }
+
+    //if fas (favorited icon class), send to localstorage, else remove
+    if($(e.target).hasClass('fas')) {
+      faves.push(obj)
+      localStorage.setItem('favorites', JSON.stringify(faves))
+    } else {
+      // iterate through the faves array, looking for the recipe you want to remove, once found, remove it with .splice
+
+      for(let i=0; i<faves.length; i++) {
+        if (faves[i].link === link) {
+          faves.splice(i, 1)
+        }
+        localStorage.setItem('favorites', JSON.stringify(faves))
+      }
+    }
   })
 }
